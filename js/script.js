7a8b6cff90d54604389d0ca14e331d58e373206b
Gengen0123 <gengen0123skay@gmail.com>
2026-06-03 20:52:50 +0900

    データベースを更新　質問事項を追加

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 // ビカクシダ原種18種のデータベース
 const platycerium = [
-  { name: "コロナリウム", water: "多め", size: "大きい", shape: "ふしぎ", obtain: "普通" },
-  { name: "グランデ", water: "多め", size: "大きい", shape: "かっこいい", obtain: "普通" },
-  { name: "ホルタミー", water: "多め", size: "大きい", shape: "ふしぎ", obtain: "難しい" },
-  { name: "リドレイ", water: "少なめ", size: "小さい", shape: "ふしぎ", obtain: "難しい" },
-  { name: "ワリチー", water: "多め", size: "大きい", shape: "ふしぎ", obtain: "難しい" },
-  { name: "ワンダエ", water: "多め", size: "大きい", shape: "かっこいい", obtain: "難しい" },
-  { name: "ビフルカツム", water: "普通", size: "普通", shape: "かっこいい", obtain: "簡単" },
-  { name: "ヒリー", water: "普通", size: "普通", shape: "かわいい", obtain: "普通" },
-  { name: "スパーバム", water: "普通", size: "大きい", shape: "かっこいい", obtain: "普通" },
-  { name: "ベイチー", water: "少なめ", size: "普通", shape: "かっこいい", obtain: "普通" },
-  { name: "ウィリンキー", water: "普通", size: "普通", shape: "かっこいい", obtain: "普通" },
-  { name: "アンディナム", water: "少なめ", size: "普通", shape: "かっこいい", obtain: "難しい" },
-  { name: "アルシコルネ", water: "普通", size: "普通", shape: "かっこいい", obtain: "普通" },
-  { name: "エレファントティス", water: "多め", size: "大きい", shape: "ふしぎ", obtain: "普通" },
-  { name: "エリシー", water: "普通", size: "小さい", shape: "かわいい", obtain: "難しい" },
-  { name: "ステマリア", water: "普通", size: "普通", shape: "かわいい", obtain: "普通" },
-  { name: "マダガスカリエンセ", water: "多め", size: "小さい", shape: "ふしぎ", obtain: "難しい" },
-  { name: "クアドリディコトマム", water: "多め", size: "小さい", shape: "ふしぎ", obtain: "難しい" },
+  { name: "コロナリウム", water: "多め", size: "大きい", shape: "ふしぎ", price: "普通", difficulty: "普通", growth: "普通" },
+  { name: "グランデ", water: "多め", size: "大きい", shape: "かっこいい", price: "普通", difficulty: "普通", growth: "普通" },
+  { name: "ホルタミー", water: "多め", size: "大きい", shape: "ふしぎ", price: "高い", difficulty: "難しい", growth: "遅い" },
+  { name: "リドレイ", water: "少なめ", size: "小さい", shape: "ふしぎ", price: "高い", difficulty: "難しい", growth: "遅い" },
+  { name: "ワリチー", water: "多め", size: "大きい", shape: "ふしぎ", price: "高い", difficulty: "難しい", growth: "遅い" },
+  { name: "ワンダエ", water: "多め", size: "大きい", shape: "かっこいい", price: "高い", difficulty: "難しい", growth: "普通" },
+  { name: "ビフルカツム", water: "普通", size: "普通", shape: "かっこいい", price: "安い", difficulty: "簡単", growth: "早い" },
+  { name: "ヒリー", water: "普通", size: "普通", shape: "かわいい", price: "普通", difficulty: "簡単", growth: "早い" },
+  { name: "スパーバム", water: "普通", size: "大きい", shape: "かっこいい", price: "普通", difficulty: "普通", growth: "普通" },
+  { name: "ベイチー", water: "少なめ", size: "普通", shape: "かっこいい", price: "普通", difficulty: "普通", growth: "普通" },
+  { name: "ウィリンキー", water: "普通", size: "普通", shape: "かっこいい", price: "普通", difficulty: "普通", growth: "普通" },
+  { name: "アンディナム", water: "少なめ", size: "普通", shape: "かっこいい", price: "高い", difficulty: "難しい", growth: "遅い" },
+  { name: "アルシコルネ", water: "普通", size: "普通", shape: "かっこいい", price: "普通", difficulty: "簡単", growth: "早い" },
+  { name: "エレファントティス", water: "多め", size: "大きい", shape: "ふしぎ", price: "普通", difficulty: "普通", growth: "普通" },
+  { name: "エリシー", water: "普通", size: "小さい", shape: "かわいい", price: "高い", difficulty: "難しい", growth: "遅い" },
+  { name: "ステマリア", water: "普通", size: "普通", shape: "かわいい", price: "普通", difficulty: "普通", growth: "普通" },
+  { name: "マダガスカリエンセ", water: "多め", size: "小さい", shape: "ふしぎ", price: "高い", difficulty: "難しい", growth: "遅い" },
+  { name: "クアドリディコトマム", water: "多め", size: "小さい", shape: "ふしぎ", price: "高い", difficulty: "難しい", growth: "遅い" },
 ];
 
 
@@ -29,6 +29,7 @@ const userAnswers = {
 // 原種と回答を比較する
 
 for (const platyceriumSample of platycerium) { // データベースの各要素を順番に変数へ格納していく繰り返し処理の書き方
+  // constは定数なので、forを回すたびに中身が変わる必要がある今回はfor文の中に入れる必要がある
   let score = 0; // 性質が一致したらスコアを加算する式の合計値を格納する　大きいほどマッチしていることになる
 
   if (platyceriumSample.water === userAnswers.water) { // ユーザーの選択と原種の性質が一致していたらスコアに1が足される
@@ -48,3 +49,5 @@ for (const platyceriumSample of platycerium) { // データベースの各要素
   console.log(score);
 
 };
+
+
